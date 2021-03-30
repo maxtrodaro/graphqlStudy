5d2f9d264af5e1b63e8c9c29d8da365a0b81d88c
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2021-03-30 00:01:11 -0300

    configure server

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log("Hello!");
+import { createServer } from "http";
+
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/status": {
+      response.writeHead(200, {
+        "Content-type": "application/json",
+      });
+      response.write(
+        JSON.stringify({
+          status: "Ok",
+        })
+      );
+      response.end();
+      break;
+    }
+    default: {
+      response.writeHead(404, "Service not found");
+      response.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || "127.0.0.1";
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`);
+});
