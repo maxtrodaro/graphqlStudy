45aee4cac39bc82f8574feb828478ca3ba224ce0
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2021-04-23 16:40:48 -0300

    implementing resolvers

--- packages/server/src/graphql/resolvers.js
+++ packages/server/src/graphql/resolvers.js
@@ -1,9 +1,12 @@
 import { resolvers as demandResolvers } from "./Demand/Demand";
+import { resolvers as clientResolvers } from "./Client/Client";
 
 const resolvers = {
   ...demandResolvers,
+  ...clientResolvers,
 
   Query: {
+    ...clientResolvers.Query,
     ...demandResolvers.Query,
   },
 };
