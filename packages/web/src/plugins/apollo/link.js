a5bd5f7df713708219668f42f25e284cc03b6442
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2021-04-27 18:42:26 -0300

    Apollo provider

---
+++ packages/web/src/plugins/apollo/link.js
@@ -0,0 +1,35 @@
+import { ApolloLink, Observable } from "apollo-link";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
+
+const loggerLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      forward(operation).subscribe({
+        next: (result) => {
+          console.log("log", result);
+          observer.next(result);
+        },
+        complete: observer.complete.bind(observer),
+        error: observer.complete.bind(observer),
+      });
+    })
+);
+
+const link = ApolloLink.from([
+  loggerLink,
+  onError((error) => {
+    console.error("GraohQLError", error);
+  }),
+  setContext((_, { headers }) => {
+    return {
+      headers,
+    };
+  }),
+  createHttpLink({
+    uri: "http://127.0.0.1:8000/graphql",
+  }),
+]);
+
+export default link;
